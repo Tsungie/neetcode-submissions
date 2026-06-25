1d39b2729b6b7c3aa92f1a0f978f360940c0a22d
Tsungie Ncube <misstsungie@gmail.com>
2026-06-25 17:39:21 +0200

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,10 @@
+integer_type,float_type,bool_type,string_type,list_type = 8,6.4,True,"Tsue",[5,7,9]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
