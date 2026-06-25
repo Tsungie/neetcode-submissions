c662b8e4dc7daec0565b067f63e1f343df06b0db
Tsungie Ncube <misstsungie@gmail.com>
2026-06-25 17:43:20 +0200

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 90
+print(type(variable))
+variable = 9.78
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "Tsue"
+print(type(variable))
+variable = [8,0,8,2]
+print(type(variable))
