9e5fc0220d60ae3e28fcd6b5b5ca5a392c06030a
Tsungie Ncube <misstsungie@gmail.com>
2026-07-22 11:58:53 +0200

    Add: python-return-statement - submission-0

---
+++ Python For Beginners/python-return-statement/submission-0.py
@@ -0,0 +1,12 @@
+def product(par_1,para_2):
+    return par_1 * para_2
+
+
+
+
+
+# don't modify below this line
+print(product(2, 4))
+print(product(8, 2))
+print(product(4, 8))
+print(product(8, 8))
